1d52eb93ed6bb76d57495dab5d46a725eee48558
b1012038 <murakamijun@murakami-jun-no-MacBook-Pro.local>
2014-10-08 00:45:21 +0900

    Initial Commit

---
+++ RhythWalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RhythWalk
+//
+//  Created by 村上 惇 on 2014/10/08.
+//  Copyright (c) 2014年 Jun Murakami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
