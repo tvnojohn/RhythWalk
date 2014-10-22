229fd06ede823c5f9fa1fd2141460a184a075072
b1012038 <murakamijun@murakami-jun-no-MacBook-Pro.local>
2014-10-22 17:18:55 +0900

    音楽再生ボタンをクリックした時の動作訂正

--- RhythWalk/ViewController.swift
+++ RhythWalk/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    var active = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // *** UIButton with Image ***
+        let image = UIImage(named: "停止ボタン.png") as UIImage
+        let image2 = UIImage(named: "再生ボタン.png") as UIImage
+        let imageButton   = UIButton()
+        imageButton.tag = 4
+        imageButton.frame = CGRectMake(0, 0, 50, 50)
+        imageButton.layer.position = CGPoint(x: self.view.frame.width/2, y:500)
+        if(active==false){
+            imageButton.setImage(image2, forState: .Normal)
+        }
+        else{
+            imageButton.setImage(image, forState: .Normal)
+        }
+        imageButton.addTarget(self, action: "tapped:", forControlEvents:.TouchUpInside)
+        
+        self.view.addSubview(imageButton)
+
+
     }
 
+    func tapped(sender: UIButton){
+        let image = UIImage(named: "停止ボタン.png") as UIImage
+        let image2 = UIImage(named: "再生ボタン.png") as UIImage
+        //let imageButton   = UIButton()
+        
+        if(active==false){
+            sender.setImage(image, forState: .Normal)
+            active = true
+        }
+        else{
+            sender.setImage(image2, forState: .Normal)
+            active = false
+        }
+        self.view.addSubview(sender)
+        //println("Tapped Button Tag:\(sender.tag)")
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
